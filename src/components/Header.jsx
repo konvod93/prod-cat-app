6de9fdd849de02918abaaa0f8871f5242ba1f612
Konstantin <konvod93@gmail.com>
2025-08-01 18:06:21 +0300

    updated Header - added Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="flex bg-blue-500 text-white p-4">
       <ul className="flex pb-2 pt-1 space-x-4 gap-4">
-        <li>Home</li>
-        <li>Products</li>
-        <li>Categories</li>
-        <li>About Us</li>
+        <li><Link to='/' className='hover:text-blue-800'>Home</Link></li>
+        <li><Link to='/Products' className='hover:text-blue-800'>Products</Link></li>
+        <li><Link to='/Categories' className='hover:text-blue-800'>Categories</Link></li>
+        <li><Link to='/About' className='hover:text-blue-800'>About Us</Link></li>
       </ul>
       <div className="relative flex items-center ml-auto">
         <input
