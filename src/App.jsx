f8979a4ed4311b5d03a67fe92a6a9cc21cb0dfcd
Konstantin <konvod93@gmail.com>
2025-08-03 10:14:53 +0300

    added Footerr in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   
 
   return (
-    <Header />  
-  )
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        {/* Main content goes here */}
+        Hello!
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App
