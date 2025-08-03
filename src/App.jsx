ad4a9e5e8f1242f38ec342bea7323a59deb69f67
Konstantin <konvod93@gmail.com>
2025-08-03 11:24:07 +0300

    added App.css, chanded index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Categories from "./components/pages/Categories";
+import About from "./components/pages/About";
+import './App.css';
+
 
 function App() {
   
@@ -7,9 +14,16 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-1 overflow-auto">
         {/* Main content goes here */}
-        Hello!
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/about" element={<About />} />
+          {/* Redirect any unknown paths to Home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>        
       </main>
       <Footer />
     </div>
