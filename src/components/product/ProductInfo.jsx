660297a0236fa916ffcce12be9cde639a21f448c
Konstantin <konvod93@gmail.com>
2026-04-29 17:22:21 +0300

    cleaned ProductInfo component

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -3,16 +3,6 @@ import { useCategories } from "../../hooks/useCategories";
 import ProductCardButton from "./ProductCardButton";
 import ProductRating from "./ProductRating";
 
-// const renderStars = (rating) => {
-//   const stars = [];
-//   const full = Math.floor(rating);
-//   const half = rating % 1 !== 0;
-//   for (let i = 0; i < full; i++) stars.push(<span key={i} className="text-yellow-400">★</span>);
-//   if (half) stars.push(<span key="half" className="text-yellow-400">☆</span>);
-//   for (let i = 0; i < 5 - Math.ceil(rating); i++) stars.push(<span key={`e${i}`} className="text-gray-300 dark:text-gray-500">★</span>);
-//   return stars;
-// };
-
 const formatPrice = (price) => {
   return new Intl.NumberFormat("ru-RU", { style: "currency", currency: "UAH" }).format(price);
 };
