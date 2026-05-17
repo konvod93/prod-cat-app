93f1470c48b208bb140d2a6db094dc6eae4758d5
Konstantin <konvod93@gmail.com>
2026-05-17 17:40:15 +0300

    refactoring of Products page: added Spinner component

---
+++ src/components/product/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-600"></div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,6 +7,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { useProductFilters } from "../hooks/useProductFilters";
 import SearchBar from "../components/product/SearchBar";
+import Spinner from "../components/product/Spinner";
 
 const Products = ({ showSearchBar = true, mode = "all" }) => {
   const { products, isLoading: isProductsLoading } = useProducts();
@@ -29,12 +30,7 @@ const Products = ({ showSearchBar = true, mode = "all" }) => {
     activeFiltersCount,
   } = useProductFilters(products, mode);
 
-  if (isProductsLoading)
-    return (
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-600"></div>
-      </div>
-    );
+  if (isProductsLoading) return <Spinner />;
 
   return (
     <div className="container mx-auto px-4 py-8">
