53d23a506f27eba30ad27955c16fd1d1953966c6
Konstantin <konvod93@gmail.com>
2026-06-30 17:01:31 +0300

    feat: added ping function

---
+++ netlify/functions/ping-supabase.js
@@ -0,0 +1,34 @@
+import fetch from "node-fetch";
+
+export async function handler() {
+  const SUPABASE_URL =
+    "https://tfgbohpqatvinfrcuzxw.supabase.co/rest/v1/categories";
+  const SUPABASE_KEY = import.meta.env.SUPABASE_ANON_KEY;
+  try {
+    const responce = await fetch(
+      `${SUPABASE_URL}/rest/v1/categories?select=id&limit=1`,
+      {
+        headers: {
+          apikey: SUPABASE_KEY,
+          Authorization: `Bearer ${SUPABASE_KEY}`,
+        },
+      },
+    );
+    if (!responce.ok) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Supabase alive" }),
+      };
+    } else {
+      return {
+        statusCode: responce.status,
+        body: JSON.stringify({ status: "Error pinging Supabase" }),
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ status: "Ping failed", error: error.message }),
+    };
+  }
+}
